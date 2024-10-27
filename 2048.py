47e1544a9ef4b33c9d6c7cd3a43cd37c4045d217
Captain Crook <76696175+gaurav1895@users.noreply.github.com>
2024-10-27 23:36:20 +0530

    Update 2048.py

--- 2048.py
+++ 2048.py
@@ -2,3 +2,12 @@ import pygame
 import random
 
 pygame.init()
+
+# intial setup
+WIDTH = 400
+HEIGHT = 500
+screen = pygame.display.set_mode([WIDTH, HEIGHT])
+pygame.display.set_caption('2048')
+timer = pygame.time.Clock()
+fps = 60
+font = pygame.font.Font('freesansbold.ttf', 24)
